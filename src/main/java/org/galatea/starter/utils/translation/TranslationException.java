6bd261f45322a7b6b3d608bea9eb72213827e247
paulo-reichert <reichert@galatea-associates.com>
2018-03-15 08:03:45 -0400

    #39 - removing unused constructors.

--- src/main/java/org/galatea/starter/utils/translation/TranslationException.java
+++ src/main/java/org/galatea/starter/utils/translation/TranslationException.java
@@ -1,18 +1,11 @@
 package org.galatea.starter.utils.translation;
 
+/**
+ * Represents an error in translating two objects.
+ */
 public class TranslationException extends RuntimeException{
-  public TranslationException() {
-  }
-
-  public TranslationException(String message) {
-    super(message);
-  }
 
   public TranslationException(String message, Throwable cause) {
     super(message, cause);
-  }
-
-  public TranslationException(Throwable cause) {
-    super(cause);
   }
 }
