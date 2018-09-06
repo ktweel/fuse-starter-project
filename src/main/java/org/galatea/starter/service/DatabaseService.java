4a416da3c6751a1ee7e8e770060af11e9bd5982e
Kathryn Tweel <tweel@galatea-associates.com>
2018-09-06 09:56:50 -0400

    remove unneeded imports

--- src/main/java/org/galatea/starter/service/DatabaseService.java
+++ src/main/java/org/galatea/starter/service/DatabaseService.java
@@ -1,9 +1,7 @@
 package org.galatea.starter.service;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.galatea.starter.domain.AlphaVantageStockPriceData;
